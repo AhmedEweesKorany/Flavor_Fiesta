9e5df281958f7b5bd32ff64ea74561b5c006c45d
Sayed Ashraf <sayedelwekel987@gmail.com>
2024-02-19 17:51:15 +0200

    Edit the sql name

--- flavor_fiesta.sql
+++ flavor_fiesta.sql
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `falvor_fiesta`
+-- Database: `flavor_fiesta`
 --
 
 -- --------------------------------------------------------
