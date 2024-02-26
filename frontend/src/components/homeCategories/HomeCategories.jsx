bea78ffce412bbf69ddea0289ad93b83f199180d
Ahmed Ewees Korany <ahmedewaisvi34@gmail.com>
2024-02-26 23:03:49 +0200

    last ypdate

--- frontend/src/components/homeCategories/HomeCategories.jsx
+++ frontend/src/components/homeCategories/HomeCategories.jsx
@@ -1,44 +1,65 @@
-import React from "react";
-import { Button, ComponentLoading, NoData, SingleCard } from "..";
-import { BsArrowUpRight } from "react-icons/bs";
-import { Link } from "react-router-dom";
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
 
-const HomeCategories = ({ title, data, isLoading }) => {
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+
+import "./style.css";
+
+// import required modules
+import { EffectCoverflow, Pagination } from "swiper/modules";
+
+export default function HomeCategories() {
   return (
     <>
-      {isLoading ? (
-        <ComponentLoading />
-      ) : (
-        <section className="box mt-28 flex flex-col items-center gap-6">
-          <div className="w-full flex justify-between items-center">
-            <h2 className="text-3xl font-bold capitalize">Latest {title}s</h2>
-            <Link to={"/recipe"}>
-              <Button
-                content={"View More"}
-                customCss={"rounded-lg text-sm"}
-                icon={<BsArrowUpRight />}
-              />
-            </Link>
-          </div>
-          <hr className="w-full" />
-          {/* Cards container */}
-          {data?.length ? (
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10">
-              {data?.slice(0, 4).map((singleData) => (
-                <SingleCard
-                  key={singleData._id}
-                  singleData={singleData}
-                  type={title}
-                />
-              ))}
-            </div>
-          ) : (
-            <NoData text={"Data"} />
-          )}
-        </section>
-      )}
+      <Swiper
+        effect={"coverflow"}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={"auto"}
+        loop={true}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        pagination={true}
+        modules={[EffectCoverflow, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
+        </SwiperSlide>
+      </Swiper>
     </>
   );
-};
-
-export default HomeCategories;
+}
